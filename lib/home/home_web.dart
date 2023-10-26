2e98fd8a5c4ed820dd738a65a2daff290933ed7d
GwangjinJeong1 <dbsusqn12@gmail.com>
2023-10-27 01:43:53 +0900

    Home Centered

--- lib/home/home_web.dart
+++ lib/home/home_web.dart
@@ -84,8 +84,7 @@ class _HomeWebState extends State<HomeWeb> {
                     if (_selectedGame == 8) {
                     } else {
                       setState(() {
-                        _selectedGame =
-                            (_selectedGame + 1) % _GameNames.length;
+                        _selectedGame = (_selectedGame + 1) % _GameNames.length;
                         scr.animateToItem(
                           _selectedGame,
                           duration: const Duration(milliseconds: 300),
@@ -110,132 +109,129 @@ class _HomeWebState extends State<HomeWeb> {
                   }
                 }
               },
-              child: Column(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    children: [
-                      contentList[_selectedGame],
-                      SizedBox(
-                        width: 635,
-                        height: 671,
-                        child: CupertinoPicker(
-                          scrollController: scr,
-                          magnification: 1.22,
-                          squeeze: 1.2,
-                          useMagnifier: true,
-                          itemExtent: 100,
-                          onSelectedItemChanged: (int selectedItem) {
-                            setState(() {
-                              _selectedGame = selectedItem;
-                              debugPrint("$_selectedGame");
-                            });
-                          },
-                          selectionOverlay: null,
-                          children: List<Widget>.generate(_GameNames.length,
-                              (int index) {
-                            final isSelected = index == _selectedGame;
-                            return Center(
-                                child: isSelected
-                                    ? Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Image.asset("assets/images/left.png",
-                                              width: 24, height: 42),
-                                          const SizedBox(width: 18),
-                                          InkWell(
-                                            onTap: () {
-                                              switch (_selectedGame + 1) {
-                                                case 1: // 인물퀴즈
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              const PersonGamePage()));
-                                                  break;
-                                                case 2: // 디스코
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              const DiscoGamePage()));
-                                                  break;
-                                                case 3: // 대표게임
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              const CaptainGamePage()));
-                                                  break;
-                                                case 4: // 네글자퀴즈
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              const FourGamePage()));
-                                                  break;
-                                                case 5: // 단어텔레파시
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              const TeleGamePage()));
-                                                  break;
-                                                case 6: // 텔레스트레이션
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              const TelestrationGamePage()));
-                                                  break;
-                                                case 7: // 액션초성게임
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              const ChoiGamePage()));
-                                                  break;
-                                                case 8: // 노래초성퀴즈
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              const ChoiGamePage()));
-                                                  break;
-                                                case 9: // 명대사퀴즈
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              const ChoiGamePage()));
-                                                  break;
-                                              }
-                                            },
-                                            child: Container(
-                                              width: 330,
-                                              height: 59,
-                                              decoration: const BoxDecoration(
-                                                  color: Color(0xFFFF62D3)),
-                                              child: Center(
-                                                child: Text(
-                                                  _GameNames[index],
-                                                  style: const TextStyle(
-                                                      fontFamily: 'DungGeunMo',
-                                                      fontSize: 44,
-                                                      fontWeight:
-                                                          FontWeight.w400,
-                                                      color: Colors.white),
-                                                ),
-                                              ),
+                  contentList[_selectedGame],
+                  SizedBox(
+                    width: 635,
+                    height: 671,
+                    child: CupertinoPicker(
+                      scrollController: scr,
+                      magnification: 1.22,
+                      squeeze: 1.2,
+                      useMagnifier: true,
+                      itemExtent: 100,
+                      onSelectedItemChanged: (int selectedItem) {
+                        setState(() {
+                          _selectedGame = selectedItem;
+                          debugPrint("$_selectedGame");
+                        });
+                      },
+                      selectionOverlay: null,
+                      children: List<Widget>.generate(_GameNames.length,
+                          (int index) {
+                        final isSelected = index == _selectedGame;
+                        return Center(
+                            child: isSelected
+                                ? Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset("assets/images/left.png",
+                                          width: 24, height: 42),
+                                      const SizedBox(width: 18),
+                                      InkWell(
+                                        onTap: () {
+                                          switch (_selectedGame + 1) {
+                                            case 1: // 인물퀴즈
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const PersonGamePage()));
+                                              break;
+                                            case 2: // 디스코
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const DiscoGamePage()));
+                                              break;
+                                            case 3: // 대표게임
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const CaptainGamePage()));
+                                              break;
+                                            case 4: // 네글자퀴즈
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const FourGamePage()));
+                                              break;
+                                            case 5: // 단어텔레파시
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const TeleGamePage()));
+                                              break;
+                                            case 6: // 텔레스트레이션
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const TelestrationGamePage()));
+                                              break;
+                                            case 7: // 액션초성게임
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const ChoiGamePage()));
+                                              break;
+                                            case 8: // 노래초성퀴즈
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const ChoiGamePage()));
+                                              break;
+                                            case 9: // 명대사퀴즈
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const ChoiGamePage()));
+                                              break;
+                                          }
+                                        },
+                                        child: Container(
+                                          width: 330,
+                                          height: 59,
+                                          decoration: const BoxDecoration(
+                                              color: Color(0xFFFF62D3)),
+                                          child: Center(
+                                            child: Text(
+                                              _GameNames[index],
+                                              style: const TextStyle(
+                                                  fontFamily: 'DungGeunMo',
+                                                  fontSize: 44,
+                                                  fontWeight:
+                                                      FontWeight.w400,
+                                                  color: Colors.white),
                                             ),
                                           ),
-                                          const SizedBox(width: 18),
-                                          Image.asset("assets/images/right.png",
-                                              width: 24, height: 42)
-                                        ],
-                                      )
-                                    : Text(_GameNames[index],
-                                        style: TextStyle(
-                                            fontFamily: 'DungGeunMo',
-                                            fontSize: 44,
-                                            fontWeight: FontWeight.w400,
-                                            color: Colors.white
-                                                .withOpacity(0.5))));
-                          }),
-                        ),
-                      ),
-                    ],
+                                        ),
+                                      ),
+                                      const SizedBox(width: 18),
+                                      Image.asset("assets/images/right.png",
+                                          width: 24, height: 42)
+                                    ],
+                                  )
+                                : Text(_GameNames[index],
+                                    style: TextStyle(
+                                        fontFamily: 'DungGeunMo',
+                                        fontSize: 44,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.white
+                                            .withOpacity(0.5))));
+                      }),
+                    ),
                   ),
                 ],
               ),
