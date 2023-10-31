d98a015b36a3c3f476a7b1059dd77cd4422e1dd6
GwangjinJeong1 <dbsusqn12@gmail.com>
2023-10-31 21:20:20 +0900

    Add keyboard interaction on splash

--- lib/musictitle/category_musictitle.dart
+++ lib/musictitle/category_musictitle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'game_musictitle.dart';
 
@@ -11,6 +12,13 @@ class CategoryPage extends StatefulWidget {
 
 class _CategoryPageState extends State<CategoryPage> {
   int selectedCategory = -1;
+  FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    focusNode.requestFocus();
+  }
 
   void _handleHover(int category) {
     setState(() {
@@ -71,55 +79,70 @@ class _CategoryPageState extends State<CategoryPage> {
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: height * 0.056),
-              Container(
-                margin:
-                    EdgeInsets.only(left: width * 0.084, right: width * 0.084),
-                child: Image.asset(
-                  'assets/images/title.png',
-                  width: width * 0.15,
-                  height: height * 0.038,
+          RawKeyboardListener(
+            focusNode: focusNode,
+            onKey: (RawKeyEvent event) {
+              if (event is RawKeyDownEvent) {
+                if (event.logicalKey == LogicalKeyboardKey.escape) {
+                  Navigator.of(context).pop();
+                }
+              }
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: height * 0.056),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(
+                        left: width * 0.084, right: width * 0.084),
+                    child: Image.asset(
+                      'assets/images/title.png',
+                      width: width * 0.15,
+                      height: height * 0.038,
+                    ),
+                  ),
                 ),
-              ),
-              SizedBox(height: height * 0.17),
-              Container(
-                margin: EdgeInsets.only(left: width * 0.085),
-                child: Row(
-                  children: [
-                    _buildCategoryWidget(
-                      category: 1,
-                      label: "1990년대",
-                      width: width,
-                      height: height,
-                    ),
-                    SizedBox(width: width * 0.024),
-                    _buildCategoryWidget(
-                      category: 2,
-                      label: "2000년대",
-                      width: width,
-                      height: height,
-                    ),
-                    SizedBox(width: width * 0.024),
-                    _buildCategoryWidget(
-                      category: 3,
-                      label: "2010년대",
-                      width: width,
-                      height: height,
-                    ),
-                    SizedBox(width: width * 0.024),
-                    _buildCategoryWidget(
-                      category: 4,
-                      label: "2020년대",
-                      width: width,
-                      height: height,
-                    ),
-                  ],
+                SizedBox(height: height * 0.17),
+                Container(
+                  margin: EdgeInsets.only(left: width * 0.085),
+                  child: Row(
+                    children: [
+                      _buildCategoryWidget(
+                        category: 1,
+                        label: "1990년대",
+                        width: width,
+                        height: height,
+                      ),
+                      SizedBox(width: width * 0.024),
+                      _buildCategoryWidget(
+                        category: 2,
+                        label: "2000년대",
+                        width: width,
+                        height: height,
+                      ),
+                      SizedBox(width: width * 0.024),
+                      _buildCategoryWidget(
+                        category: 3,
+                        label: "2010년대",
+                        width: width,
+                        height: height,
+                      ),
+                      SizedBox(width: width * 0.024),
+                      _buildCategoryWidget(
+                        category: 4,
+                        label: "2020년대",
+                        width: width,
+                        height: height,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
